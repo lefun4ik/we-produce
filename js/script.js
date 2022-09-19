3af80ba26a9a2a288e242816b29838e1f2b2673f
lefun4ik <alexey.tsvirkun17@gmail.com>
2022-09-19 17:57:57 +0300

    сделал слайдер + ховеры на кнопки

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
+$(document).ready(function(){
+	$('.slider').slick({
+		speed: 500,
+		cssEase: 'cubic-bezier(0.39, 0.575, 0.565, 1)'
+	});
+})
+
+
+
 const play = document.querySelector('.about__play');
 const video = document.querySelector('.about__video video');
 
